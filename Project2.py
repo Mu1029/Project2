74e1ea653f0b2e074d13c1217a5e32bcf8da9dfb
Mu1029 <muayad.abdel@torontomu.ca>
2023-11-10 04:41:01 -0500

    Create Project2.py
    input img shape

---
+++ Project2.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+import numpy as np
+from keras import ImageDataGenerator, models, layers
+
+
+#  Step 1
+
+    # defining input image shape 
+    
+image_shape = (100, 100, 3)
+
+
+
